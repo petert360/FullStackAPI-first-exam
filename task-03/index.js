0b34848d89e836e2d12a77d41781348a80b5feef
petert360 <peter.t360@hotmail.com>
2021-07-05 15:28:39 +0200

    Harmadik feladat megoldása

--- task-03/index.js
+++ task-03/index.js
@@ -3,12 +3,15 @@
  * emeld ki egy változóba az update metódusát.
  */
 
+const { update } = require('./jsonHandler');
+
+
 /**
  * 2. Ebben a névtelen aszinkron függvényben futtatsd az update metódust. 
  * Ha készen van a json kezelő modulod, akkor csupán élesítened kell az alábbi 
  * kódot a komment jelek eltávolításával.
  */
-/*
+
 ( async () => {
     console.log( await update({
         "id": 7,
@@ -17,4 +20,3 @@
         "bestBefore": "2021-05-30"
     }));
 })();
-*/
--- task-03/jsonHandler.js
+++ task-03/jsonHandler.js
@@ -2,11 +2,15 @@
  * 1. A fájlok kezeléséhez az fs modul promise alapú verzióját használd.
  */
 
+const fsp = require('fs').promises;
+const { join } = require('path');
+
+
 /**
  * 2. Állítsd be az azonos mappában található .json fájl elérési útját a path 
  * modul join metódusának segítségével.
  */
- const jsonPath = '';
+const jsonPath = join(__dirname, 'db', 'products.json');
 
  /**
   * 3. A jsonPath útvonalon található fájl tartalmát beolvassa és értelmezi, 
@@ -14,7 +18,8 @@
   * @returns objektumok tömbje
   */
  const getList = async () => {
-     //
+    const fileContent = await fsp.readFile(jsonPath, 'utf8');
+    return JSON.parse(fileContent);
  };
 
 /**
@@ -23,8 +28,11 @@
  * @param {Array} list objektumok tömbje
  * @returns 
  */
+
+// Létrehozunk egy külön aszinkron függvényt a kiírásra
 const saveList = async (list = []) => {
-    //
+    await fsp.writeFile(jsonPath, JSON.stringify(list, null, 2), 'utf8');
+    return true;
 };
 
 /**
@@ -35,7 +43,16 @@ const saveList = async (list = []) => {
  * @returns a frissített objektum ha sikerült a frissítés, egyébként false
  */
 const update = async (entity = {}) => {
-    //
+    // hiba esetén false válasz
+    if (!entity.id) return false;
+    // beolvassuk a product listát
+    const productList = await getList();
+    // megkeressük az id alapján a tömb indexét, és a lista adott indexű eleme legyen egyelő a kapott entitással.
+    const idx = productList.findIndex(item => item.id === entity.id);
+    productList[idx] = { ...productList[idx], ...entity };
+    // saveList segítségével kiírjuk az új listát
+    await saveList(productList);
+    return productList[idx];
 };
 
 module.exports = {
